5136cb8d615aab73c1ec3fc9abecfeb47a16138a
ThePrior <41752563+ThePrior@users.noreply.github.com>
2018-08-06 13:55:41 +0100

    add members to groups working

--- awpUtils.js
+++ awpUtils.js
@@ -67,7 +67,7 @@ window.AWP.CreateMeetingSites = function () {
         return deferred.promise();
     },
 
-    createGroup = function (webName, groupTitle, groupDescription, SPRoleType) {
+    createGroup = function (webName, groupTitle, groupDescription, SPRoleType, users) {
 
         console.log("in createGroup: webName = " + webName + ", groupTitle = " + groupTitle);
 
@@ -87,14 +87,14 @@ window.AWP.CreateMeetingSites = function () {
         collRollAssignment = web.get_roleAssignments();
         collRollAssignment.add(newGroup, collRoleDefinitionBinding);
 
-        //Add users. TODO: Users will be passedin as an array.
-        /*
-        user = web.ensureUser('rvn39417');
-		
-        var userCollection = newGroup.get_users();
-        userCollection.addUser(user);
-        context.load(user);
-        */
+        users.forEach(function(userName) {
+        	console.log("adding user: " + userName);
+	        user = web.ensureUser(userName);
+			
+	        var userCollection = newGroup.get_users();
+	        userCollection.addUser(user);
+	        context.load(user);
+        });
 
         context.load(newGroup);
         context.load(oRoleDefinition, 'Name'); // TODO - CHECK THIS OUT!!
@@ -161,7 +161,8 @@ window.AWP.CreateMeetingSites = function () {
 	    	var meetingSite = {};
 
 	    	meetingSite.admins = [];
-	    	meetingSite.users = [];
+	    	meetingSite.members = [];
+	    	meetingSite.visitors = [];
 	    	meetingSite.committeeName = item.get_item('Title');
 	    	meetingSite.siteTitle = item.get_item('SiteTitle');
 	    	
@@ -170,7 +171,7 @@ window.AWP.CreateMeetingSites = function () {
 	    	});
 	    	
 	    	item.get_item('User').forEach(function (user) {
-	    		meetingSite.users.push(user.get_lookupValue());
+	    		meetingSite.members.push(user.get_lookupValue());
 	    	});
 
 			meetingSites.push(meetingSite);
@@ -188,9 +189,6 @@ window.AWP.CreateMeetingSites = function () {
 		});
 		
 
-		//TODO: get unique users from admins and users and ensure they exist.
-		//		see: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-
 	    //Need async 'waterfall loop' to submit print requests for all forms for each patient, one patient at a time.
 	    //See: http://stackoverflow.com/questions/15504921/asynchronous-loop-of-jquery-deferreds-promises
 	
@@ -205,15 +203,13 @@ window.AWP.CreateMeetingSites = function () {
 
 	        // add to the $.Deferred chain with $.then() and re-assign
 	        dfd = dfd.then(function () {
-	        	var webName = site.committeeName;
-	        	var webDesc = "my desc";
 	        	
 			    waitModal = SP.UI.ModalDialog.showWaitScreenWithNoClose(dialogTitle, 
 								    dialogMessage + "'" + site.siteTitle + "'", dialogHeight, dialogWidth);
 
-		    	console.log(JSON.stringify(site));
+		    	console.log(JSON.stringify(site));    	
 		    	
-		    	return createWebAndDefaultGroups(webName, site.siteTitle, webDesc, templateGuidName, inheritPermissions);
+	    		return createWebFromMeetingSite (site, templateGuidName, inheritPermissions);
 		    }); 
 	        	        
 		});
@@ -221,11 +217,27 @@ window.AWP.CreateMeetingSites = function () {
 		return dfd.promise();
 	},
 	
-	createWebAndDefaultGroups = function(webName, siteTitle, webDesc, templateGuidName, inheritPermissions){
+	createWebFromMeetingSite = function(site, templateGuidName, inheritPermissions){
+    	var webName = site.committeeName;
+    	var webDesc = "my desc";
+
+		return createWebAndDefaultGroups(site.committeeName, site.siteTitle, webDesc, templateGuidName, inheritPermissions, 
+												site.admins, site.members, site.visitors);
+	},
+	
+	createSingleWeb = function(webName, webTitle, webDesc, templateGuidName, inheritPermissions){
+		var admins = [],
+			members = [],
+			visitors = [];
+		
+		return createWebAndDefaultGroups(webName, webTitle, webDesc, templateGuidName, inheritPermissions, admins, members, visitors);
+	},
+	
+	createWebAndDefaultGroups = function(webName, siteTitle, webDesc, templateGuidName, inheritPermissions, admins, members, visitors){
 			    	
 	            return createWeb(webName, siteTitle, webDesc, templateGuidName, inheritPermissions)
 	            .then(function(){
-	                return createGroup(webName, "Visitors", "Visitors Group", SP.RoleType.reader)
+	                return createGroup(webName, "Visitors", "Visitors Group", SP.RoleType.reader, visitors)
 	                    .then(function (group) {
 	                            return assocGroup(group, "set_associatedVisitorGroup");
 	                        }
@@ -233,7 +245,7 @@ window.AWP.CreateMeetingSites = function () {
 	            	}
 	            )
 	            .then(function(){
-	                return createGroup(webName, "Members", "Members Group", SP.RoleType.editor)
+	                return createGroup(webName, "Members", "Members Group", SP.RoleType.editor, members)
 	                    .then(function (group) {
 	                            return assocGroup(group, "set_associatedMemberGroup");
 	                        }
@@ -241,7 +253,7 @@ window.AWP.CreateMeetingSites = function () {
 	            	}
 	            )
 	            .then(function(){
-	                return createGroup(webName, "Owners", "Owners Group", SP.RoleType.administrator)
+	                return createGroup(webName, "Owners", "Owners Group", SP.RoleType.administrator, admins)
 	                    .then(function (group) {
 	                            return assocGroup(group, "set_associatedOwnerGroup");
 	                        }
@@ -286,7 +298,7 @@ window.AWP.CreateMeetingSites = function () {
 			    waitModal = SP.UI.ModalDialog.showWaitScreenWithNoClose(dialogTitle, 
 				    dialogMessage + "'" + webTitle + "'", dialogHeight, dialogWidth);
 
-	            return createWebAndDefaultGroups(webName, webTitle, "my desc", templateGuidName, inheritPermissions);
+	            return createSingleWeb(webName, webTitle, "my desc", templateGuidName, inheritPermissions);
 	        });
         }
 
